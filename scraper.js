a60d5943e59903658863fb2fc8b3e5e3aa89b317
HP <hevtiq@gmail.com>
2021-05-02 21:22:18 +0700

    add scraper

--- scraper.js
+++ scraper.js
@@ -10,13 +10,26 @@ const { url } = require('./config');
 // ;()();
 ;(async () => {
     // open browser
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
     // setup browser
     await page.setDefaultTimeout(10000);  // timeout 10s
     await page.setViewport({ width: 1200, height: 800});
     await page.setUserAgent(random_useragent.getRandom());
+
+    // get data from books
+    const name_selector = ".product_main > h1";
+    const price_selector = ".price_color";
+    await page.goto(url);
+    await page.waitForSelector(name_selector);
+    await page.waitForSelector(price_selector);
+    const name = await page.$eval(name_selector, e => e.innerHTML);
+    const price = await page.$eval(price_selector, e => e.innerHTML);
+    const nameTrim = name.trim();
+    const priceTrim = price.trim();
+
+    console.log({nameTrim, priceTrim});
 
     // close browser
     await browser.close();
